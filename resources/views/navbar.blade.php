c8a45820f967897de4ece2c30197185e3ec29d26
julie chung <julie.minjung.chung@gmail.com>
2021-10-21 21:56:45 -0400

    changed app name

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg" style="background-color: #87CEEB">
-  <a style="margin-left: 1vw;" class="navbar-brand" href="#">Book Data</a>
+  <a style="margin-left: 1vw;" class="navbar-brand" href="#">Online Bookshelf</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
